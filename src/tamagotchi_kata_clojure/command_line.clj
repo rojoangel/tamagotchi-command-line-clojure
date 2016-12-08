131b9ba55a82103a9b919b963a3b620274a3d246
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-12-08 12:21:02 +0100

    implement create via command line

--- src/tamagotchi_kata_clojure/command_line.clj
+++ src/tamagotchi_kata_clojure/command_line.clj
@@ -1,11 +1,32 @@
 (ns tamagotchi-kata-clojure.command-line
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str])
+  (:use [tamagotchi-kata-clojure.core :as tamagotchi]))
 
 (defn prompt-menu []
-  (println "create <name> : creates a new tamagotchi")
-  (println "show : shows tamagotchi")
-  (println "feed : feeds tamagotchi")
-  (println "quit : exit"))
+  (println "create [name] : creates a new tamagotchi"))
+
+(defn show-status [tamagotchi]
+  (println "name:" (:name @tamagotchi)
+           " | hungriness:" (:hungriness @tamagotchi)
+           " | fullness:" (:fullness @tamagotchi)
+           " | happiness:" (:happiness @tamagotchi)
+           " | tiredness:" (:tiredness @tamagotchi)
+           ))
+
+(defn dispatch [command arguments]
+  (case command
+    :create
+    (let [name (first arguments)
+          tamagotchi (if (nil? name)
+                       (tamagotchi/create)
+                       (tamagotchi/create :name name))]
+      (show-status tamagotchi))
+    (do
+      (println "Valid commands are: create [name]"))))
 
 (defn -main [& args]
-  (prompt-menu))
+  (prompt-menu)
+  (let [[command-str & arguments] (str/split (read-line) #" ")
+        command (keyword command-str)]
+    (dispatch command arguments)))

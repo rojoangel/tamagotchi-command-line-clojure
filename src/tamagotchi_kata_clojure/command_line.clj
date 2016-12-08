2c2bcf5aec1117da8ad386c28fa83f58fb2aca24
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-12-08 12:52:57 +0100

    implement show via command-line

--- src/tamagotchi_kata_clojure/command_line.clj
+++ src/tamagotchi_kata_clojure/command_line.clj
@@ -4,7 +4,8 @@
   (:use [tamagotchi-kata-clojure.core :as tamagotchi]))
 
 (defn prompt-menu []
-  (println "create [name] : creates a new tamagotchi"))
+  (println "create [name] : creates a new tamagotchi")
+  (println "show          : shows your tamagotchi status"))
 
 (defn show-status [tamagotchi]
   (println "name:" (:name @tamagotchi)
@@ -14,19 +15,32 @@
            " | tiredness:" (:tiredness @tamagotchi)
            ))
 
-(defn dispatch [command arguments]
+(declare ui-loop)
+
+(defn dispatch [tamagotchi command arguments]
   (case command
     :create
     (let [name (first arguments)
           tamagotchi (if (nil? name)
                        (tamagotchi/create)
                        (tamagotchi/create :name name))]
+      (dispatch tamagotchi :show nil))
+
+    :show
+    (if (nil? tamagotchi)
+      (println "You don't have a tamagotchi. Please create one with 'create'")
       (show-status tamagotchi))
+
     (do
-      (println "Valid commands are: create [name]"))))
+      (println "Valid commands are: create [name] | show")))
+
+  (ui-loop tamagotchi))
+
+(defn ui-loop [tamagotchi]
+  (let [[command-str & arguments] (str/split (read-line) #" ")
+        command (keyword command-str)]
+    (dispatch tamagotchi command arguments)))
 
 (defn -main [& args]
   (prompt-menu)
-  (let [[command-str & arguments] (str/split (read-line) #" ")
-        command (keyword command-str)]
-    (dispatch command arguments)))
+  (ui-loop nil))

b0a1233241436bc6f6e1572dae3676e61dacc5d2
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-12-18 12:54:23 +0100

    replace let with do

--- test/tamagotchi_kata_clojure/core_test.clj
+++ test/tamagotchi_kata_clojure/core_test.clj
@@ -8,17 +8,20 @@
     "about feeding"
     (fact
       "When I feed it, it's hungriness is decreased"
-      (let [tamagotchi (tamagotchi/create :hungriness 6)]
+      (do
+        (tamagotchi/create :hungriness 6)
         (:hungriness (tamagotchi/feed)) => 5))
     (fact
       "When I feed it, it's fullness is increased"
-      (let [tamagotchi (tamagotchi/create :fullness 6)]
+      (do
+        (tamagotchi/create :fullness 6)
         (:fullness (tamagotchi/feed)) => 7)))
   (facts
     "about playing"
     (fact
       "When I play with it, it's happiness is increased"
-      (let [tamagotchi (tamagotchi/create :happiness 6)]
+      (do
+        (tamagotchi/create :happiness 6)
         (:happiness (tamagotchi/play)) => 7))
     (fact
       "When I play with it, it's tiredness is increased"
@@ -28,25 +31,30 @@
     "about putting to bed"
     (fact
       "When i put it to bed, it's tiredness is decreased"
-      (let [tamagotchi (tamagotchi/create :tiredness 6)]
+      (do
+        (tamagotchi/create :tiredness 6)
         (:tiredness (tamagotchi/put-to-bed)) => 5)))
   (facts
     "about making poop"
     (fact
       "When i make it poop, it's fullness is decreased"
-      (let [tamagotchi (tamagotchi/create :fullness 6)]
+      (do
+        (tamagotchi/create :fullness 6)
         (:fullness (tamagotchi/make-poop)) => 5)))
   (facts
     "about changing needs over time"
     (fact
       "When time passes, its tiredness is increased"
-      (let [tamagotchi (tamagotchi/create :tiredness 6)]
+      (do
+        (tamagotchi/create :tiredness 6)
         (:tiredness (tamagotchi/tic)) => 7))
     (fact
       "When time passes, its hungriness is increased"
-      (let [tamagotchi (tamagotchi/create :hungriness 6)]
+      (do
+        (tamagotchi/create :hungriness 6)
         (:hungriness (tamagotchi/tic)) => 7))
     (fact
       "When time passes, its happiness is decreased"
-      (let [tamagotchi (tamagotchi/create :happiness 6)]
+      (do
+        (tamagotchi/create :happiness 6)
         (:happiness (tamagotchi/tic)) => 5))))

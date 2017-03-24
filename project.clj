5a92908774e5f81663ec8cf8c75ab52a1f530a73
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-24 10:34:25 +0100

    add overtone/at-at dependency to project

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject tamagotchi-kata-clojure "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clansi "1.0.0"]]
+                 [clansi "1.0.0"]
+                 [overtone/at-at "1.2.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}}
   :main "tamagotchi-kata-clojure.command-line")
   
